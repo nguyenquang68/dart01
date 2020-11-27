ad77bde83c1b0957d5475cfec58d9e4d8d178194
nguyenquag68 <nguyenquang11111999@gmail.com>
2020-11-27 14:29:54 +0700

    Add lab1

---
+++ bin/dartlab3.dart
@@ -0,0 +1,39 @@
+import 'dart:math'; // nạp thư viện toán
+
+void main(List<String> arguments) {
+  //print('Hello world!');
+  /* print('xin chào a');
+  print('xin chào b');
+  print('xin chào c');
+  print('xin chào a');
+  print('xin chào a');
+  */
+  //var p = 9;
+  //print('Kiểu của p là ${p.runtimeType}');
+  // var a = '10';
+  // int num = int.parse(a);
+  // print(num);
+  // var a = '10.0';
+  // int num;
+  // try {
+  //   int num = int.parse(a);
+  //   print(num);
+  // } catch (e) {
+  //   print(e.toString());
+  // }
+  // var a = 'có';
+  // final b = ' Dart $a phân biệt chữ hoa và chữ thường';
+  // const c = 'Dart có phân biệt chữ hoa và chữ thường';
+  // print(b);
+  // a = 'không';
+  // print(b);
+  hello();
+}
+
+void hello() {
+  print('xin chào a');
+  print('xin chào b');
+  print('xin chào c');
+  print('xin chào a');
+  print('xin chào a');
+}
